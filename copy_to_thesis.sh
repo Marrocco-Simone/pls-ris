6743624f60c17fd316642f9fca3a6b8909b26c36
Marrocco-Simone <marrocco.simone00@gmail.com>
2025-03-01 01:01:24 +0100

    automatic conversion from paper to thesis

---
+++ copy_to_thesis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Create thesis/imgs directory if it doesn't exist
+mkdir -p ./thesis/imgs
+
+# Copy images directory
+cp -r ./paper/imgs/* ./thesis/imgs/
+
+# Copy references.bib
+cp ./paper/references.bib ./thesis/
+
+# Create temporary file to store tex filenames
+temp_file=$(mktemp)
+
+# Copy tex files except main.tex and store filenames
+for file in ./paper/*.tex; do
+    filename=$(basename "$file")
+    if [ "$filename" != "main.tex" ]; then
+        cp "$file" "./thesis/"
+        echo "$filename" >> "$temp_file"
+    fi
+done
+
+# Clean start.tex if it already exists
+if [ -f ./thesis/start.tex ]; then
+    rm ./thesis/start.tex
+fi
+
+# Sort filenames and create start.tex with input commands
+sort "$temp_file" | while read -r filename; do
+    echo "\\input{${filename%.*}}" >> ./thesis/start.tex
+done
+
+# Replace section commands in copied tex files
+# for file in $(cat "$temp_file"); do
+#     sed -i 's/\\section/\\chapter/g' "./thesis/$file"
+#     sed -i 's/\\subsection/\\section/g' "./thesis/$file"
+#     sed -i 's/\\subsubsection/\\subsection/g' "./thesis/$file"
+# done
+
+# Update .gitignore
+echo "# Copied files" > ./thesis/.gitignore
+echo "imgs/" >> ./thesis/.gitignore
+echo "start.tex" >> ./thesis/.gitignore
+echo ".gitignore" >> ./thesis/.gitignore
+echo "references.bib" >> ./thesis/.gitignore
+sort "$temp_file" | while read -r filename; do
+    echo "$filename" >> ./thesis/.gitignore
+done
+
+# Remove temporary file
+rm "$temp_file"
+
+# cd ./thesis
+# pdflatex marrocco_simone_computer_science_2024_2025.tex
+# bibtex marrocco_simone_computer_science_2024_2025.aux
+# pdflatex marrocco_simone_computer_science_2024_2025.tex
+# pdflatex marrocco_simone_computer_science_2024_2025.tex
